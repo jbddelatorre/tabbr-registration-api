3c814a84cd877068471a39e8b0d805f48e878c64
Jason Dela Torre <jasondelatorre92@gmail.com>
2019-01-11 12:45:56 +0800

    feat(server) allowed cross origin*

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const app = express();
 //Morgan
 app.use(morgan('dev'));
 
+
+//CORS **STUDY THIS
+// *** FIX THIS BEFORE DEPLOYMENT
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Body Parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
