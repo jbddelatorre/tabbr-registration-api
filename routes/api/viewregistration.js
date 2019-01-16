05b4781450bb1aefbb5bc7f32c24c6be30deb9ca
Jason Dela Torre <jasondelatorre92@gmail.com>
2019-01-16 20:28:46 +0800

    feat(routes) /update route

--- routes/api/viewregistration.js
+++ routes/api/viewregistration.js
@@ -7,6 +7,7 @@ const Representative = require('../../models/Representative');
 router.post('/', (req, res) => {
 	const { body } = req
 	let errors = {}
+
 	Representative.findOne({
 		email: body.email,
 		registration_code: body.registration_code
@@ -18,6 +19,48 @@ router.post('/', (req, res) => {
 			}
 			return res.json(registration)
 		})
+})
+
+router.post('/update', (req, res) => {
+	const { body } = req
+	
+	Representative.findOne({ 
+		email: body.email,
+		registration_code: body.registration_code
+	})
+		.then(rep => {
+			const fields = Object.keys(rep.toJSON())
+			if(fields.includes(body.field)) {
+				return Promise.resolve(rep)
+			}
+			else return res.status(404).json({errors: "Invalid field input"})
+		})
+		.then(rep => {
+			console.log('here' + rep)
+			if(rep) {
+				Representative.findOneAndUpdate(
+					{
+						email: body.email,
+						registration_code: body.registration_code
+					},
+					{
+						$set: {
+							[body.field]: body.value,
+						}
+					},
+					{
+						new: true
+					}
+				)
+					.then(up => res.json(up))
+
+			} else {
+				return res.status(404).json({errors: "Registration not found"})
+			}
+		})
+		.catch(err => res.status(404).json(err))
+
+
 })
 
 module.exports = router;
